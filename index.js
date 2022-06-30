c9dd179a2cf5f8baa4c2c0b5536db0a25263abd9
cocoprog <morales.federico14@gmail.com>
2022-06-30 16:18:45 -0300

    desafio complementario 1

---
+++ index.js
@@ -0,0 +1,10 @@
+
+let nombre = prompt("ingrese su nombre");
+
+if(nombre === "fede"){
+  console.log("Acceso correcto");
+  alert("Acceso correcto");
+}else{
+  console.log("Acceso denegado");
+  alert("Acceso denegado");
+  }
